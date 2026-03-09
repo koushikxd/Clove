afe032b0d10a80ea547d6d45c3fc9e25733f8ffb
Koushik <koushikyemula@gmail.com>
2026-03-09 19:18:39 +0530

    feat: add Convex API, better-auth, and client provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,11 @@
 import { Geist, Geist_Mono, Nunito_Sans } from "next/font/google"
 
 import "./globals.css"
+import { ConvexClientProvider } from "@/components/convex-client-provider"
 import { ThemeProvider } from "@/components/theme-provider"
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
 
-const nunitoSans = Nunito_Sans({variable:'--font-sans'})
+const nunitoSans = Nunito_Sans({ variable: "--font-sans" })
 
 const fontMono = Geist_Mono({
   subsets: ["latin"],
@@ -20,10 +21,17 @@ export default function RootLayout({
     <html
       lang="en"
       suppressHydrationWarning
-      className={cn("antialiased", fontMono.variable, "font-sans", nunitoSans.variable)}
+      className={cn(
+        "antialiased",
+        fontMono.variable,
+        "font-sans",
+        nunitoSans.variable
+      )}
     >
       <body>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ConvexClientProvider>
+          <ThemeProvider>{children}</ThemeProvider>
+        </ConvexClientProvider>
       </body>
     </html>
   )
